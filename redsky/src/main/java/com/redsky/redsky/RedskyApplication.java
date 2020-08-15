3adfe46e4226e83129d59836c7f083a4ca5a5fbd
unknown <pandey.anjana@gmail.com>
2020-08-15 20:06:15 +0530

    add redsky

---
+++ redsky/src/main/java/com/redsky/redsky/RedskyApplication.java
@@ -0,0 +1,13 @@
+package com.redsky.redsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedskyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedskyApplication.class, args);
+	}
+
+}
